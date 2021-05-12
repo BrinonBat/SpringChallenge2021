fecfb57b660d41758c7ca4c0a0ddfd025d5f3e44
BrinonBat <baptiste@brinon.eu>
2021-05-12 16:35:21 +0200

    orga

--- src/test/java/Spring2021.java
+++ src/test/java/Spring2021.java
@@ -19,15 +19,28 @@ public class Spring2021 {
         "python3", "config/level2/Boss.py"
     };
     static String[] MY_AI = new String[] {
-        "java", "config/IaTraining.java"
+        "java", "config/MyAi/IaTraining.java"
     };
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        launchGame();
-
+        boolean repeat=true;
+        if(repeat){
+            int nb_win=0;
+            int nb_lose=0;
+            for(int nb_game=0;nb_game<100;nb_game++){
+                GameResult result=launchGame(repeat);
+                for(int i=0;i<result.agents.size();i++){
+                    System.out.println(result.agents.get(i).name+" -> "+result.scores.get(i));
+                }
+                if(result.scores.get(1)>result.scores.get(0))nb_win++;
+                else nb_lose++;
+            }
+            System.out.println(nb_win+" wins & "+nb_lose+" loses");
+        }
+        else launchGame(repeat);
     }
 
-    public static void launchGame() throws IOException, InterruptedException {
+    public static GameResult launchGame(boolean repeat) throws IOException, InterruptedException {
 
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
         gameRunner.setLeagueLevel(3);
@@ -44,24 +57,18 @@ public class Spring2021 {
             "My Ai",
             "https://static.codingame.com/servlet/fileservlet?id=61910307869345"
         );
-        /*
-        gameRunner.addAgent(
-            DEFAULT_AI,
-            "Ghilbib",
-            "https://static.codingame.com/servlet/fileservlet?id=61910289640958"
-        );
-        */
 
         long leftLimit = 1L;
         long rightLimit = 10L;
         long generatedLong = leftLimit + (long) (Math.random() * (rightLimit - leftLimit));
         gameRunner.setSeed(generatedLong);
 
-        //gameRunner.start(8888);
-        GameResult result=gameRunner.simulate();
-        for(int i=0;i<result.agents.size();i++){
-            System.out.println(result.agents.get(i).name+" -> "+result.scores.get(i));
+        if(repeat){
+            GameResult result=gameRunner.simulate();
+            return result;
         }
+        gameRunner.start(8888);
+        return null;
         
     }
 }
